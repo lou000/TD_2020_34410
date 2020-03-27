622f044ece0780df98458a691602ddadbc1543a7
lou000 <44552514+lou000@users.noreply.github.com>
2020-03-27 02:08:29 +0100

    number fiddling

--- zoomablechartview.h
+++ zoomablechartview.h
@@ -11,17 +11,18 @@ public:
     using QChartView::QChartView;
 
 protected:
-    //NOTE: this was far easier then expected
+    //NOTE: this was far easier then expected, zoomIn and zoomOut are still not equal!
     void wheelEvent(QWheelEvent *event) override{
         QPoint numDegrees = event->angleDelta() / 8;
         QPoint numSteps = numDegrees;
         QPointF mousePos = event->position();
+        double zoomFactor = static_cast<double>(numSteps.y());
         QPointF center = QPointF(chart()->rect().width()/2,chart()->rect().height()/2);
         if(numSteps.y()>0)
-            chart()->scroll(-(center.x()-mousePos.x())/6, (center.y()-mousePos.y())/6);
+            chart()->scroll(-(center.x()-mousePos.x())/(abs(zoomFactor)/2), (center.y()-mousePos.y())/(abs(zoomFactor)/2));
         else
-            chart()->scroll((center.x()-mousePos.x())/6, -(center.y()-mousePos.y())/6);
-        chart()->zoom(1+static_cast<double>(numSteps.y())/100);
+            chart()->scroll((center.x()-mousePos.x())/(abs(zoomFactor)/2), -(center.y()-mousePos.y())/(abs(zoomFactor)/2));
+        chart()->zoom(1+zoomFactor/100);
         event->accept();
     }
 
